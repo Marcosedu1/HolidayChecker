51a880f8d1a8d7812c1952225cc39fac7a6867fc
Marcos Eduardo <marcos.ec2002@gmail.com>
2022-08-03 16:12:12 -0300

    Final Version

--- CheckHoliday.cs
+++ CheckHoliday.cs
@@ -22,12 +22,12 @@
 
             List<Holiday> holidays = new List<Holiday>
             {
-                new Holiday(easter.Day,easter.Month,"Easter"),
+                new Holiday(1,1,"New Year's Day"),
                 new Holiday(carnival.Day,carnival.Month, "Carnival"),
                 new Holiday(easter.AddDays(-2).Day,easter.AddDays(-2).Month,"Good Friday"),
-                new Holiday(easter.AddDays(60).Day,easter.AddDays(60).Month,"Corpus Christi"),
+                new Holiday(easter.Day,easter.Month,"Easter"),
                 new Holiday(21,4,"Tiradentes Holiday"),
-                new Holiday(1,1,"New Year's Day"),
+                new Holiday(easter.AddDays(60).Day,easter.AddDays(60).Month,"Corpus Christi"),
                 new Holiday(7,9, "Independence of Brazil"),
                 new Holiday(12,10,"Day of Our Lady"),
                 new Holiday(2,11, "All Souls' Day"),
